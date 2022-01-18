e9663d943514a44a40362d1d9a040bd862102b38
tekdoken <tekdokenzx@gmail.com>
2022-01-18 16:12:30 +0700

    bai 7 th_3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {TimelinesComponent} from './timelines/timelines.component';
 import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
 import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
-const routes: Routes = [  {
+import {ProductListComponent} from "./product/product-list/product-list.component";
+import {ProductCreateComponent} from "./product/product-create/product-create.component";
+
+const routes: Routes = [{
   path: 'timelines',
   component: TimelinesComponent
-},  {
+}, {
   path: 'youtube',
   component: YoutubePlaylistComponent, children: [{
     path: ':id',
     component: YoutubePlayerComponent
   }]
+},{
+  path: 'product/list',
+  component: ProductListComponent
+}, {
+  path: 'product/create',
+  component: ProductCreateComponent
 }
 ];
 
@@ -19,4 +28,5 @@ const routes: Routes = [  {
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,8 @@ import { LoginComponent } from './login/login.component';
 import { TimelinesComponent } from './timelines/timelines.component';
 import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
 
 // @ts-ignore
 // @ts-ignore
@@ -49,7 +51,9 @@ import { YoutubePlayerComponent } from './youtube-player/youtube-player.componen
     LoginComponent,
     TimelinesComponent,
     YoutubePlaylistComponent,
-    YoutubePlayerComponent
+    YoutubePlayerComponent,
+    ProductListComponent,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
