77e8e62aec4d02864311aca93545254cb75ee681
tekdoken <tekdokenzx@gmail.com>
2022-01-20 10:58:52 +0700

    bai 9 CRUD 2 database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {FontSizeEditorComponent} from './font-size-editor/font-size-editor.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { PetComponent } from './pet/pet.component';
-import { ProductComponent } from './product/product.component';
-import { CalculatorComponent } from './calculator/calculator.component';
-import { ColorComponent } from './color/color.component';
-import { PromiseComponent } from './promise/promise.component';
-import { ArticleComponent } from './article/article.component';
-import { LikeComponent } from './like/like.component';
-import { NameCardComponent } from './name-card/name-card.component';
-import { ProgressBarComponent } from './progress-bar/progress-bar.component';
-import { FormComponent } from './form/form.component';
-import { FormproductComponent } from './formproduct/formproduct.component';
-import { RatingBarComponent } from './rating-bar/rating-bar.component';
-import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
-import { TodoComponent } from './todo/todo.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { TimelinesComponent } from './timelines/timelines.component';
-import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
-import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
-import { ProductListComponent } from './product/product-list/product-list.component';
-import { ProductCreateComponent } from './product/product-create/product-create.component';
-import { DictionaryListComponent } from './dictionary/dictionary-list/dictionary-list.component';
-import { DictionaryDetailComponent } from './dictionary/dictionary-detail/dictionary-detail.component';
-import { ProductUpdateComponent } from './product/product-update/product-update.component';
+import {PetComponent} from './pet/pet.component';
+import {ProductComponent} from './product/product.component';
+import {CalculatorComponent} from './calculator/calculator.component';
+import {ColorComponent} from './color/color.component';
+import {PromiseComponent} from './promise/promise.component';
+import {ArticleComponent} from './article/article.component';
+import {LikeComponent} from './like/like.component';
+import {NameCardComponent} from './name-card/name-card.component';
+import {ProgressBarComponent} from './progress-bar/progress-bar.component';
+import {FormComponent} from './form/form.component';
+import {FormproductComponent} from './formproduct/formproduct.component';
+import {RatingBarComponent} from './rating-bar/rating-bar.component';
+import {CountdownTimerComponent} from './countdown-timer/countdown-timer.component';
+import {TodoComponent} from './todo/todo.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {TimelinesComponent} from './timelines/timelines.component';
+import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
+import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {ProductListComponent} from './product/product-list/product-list.component';
+import {ProductCreateComponent} from './product/product-create/product-create.component';
+import {DictionaryListComponent} from './dictionary/dictionary-list/dictionary-list.component';
+import {DictionaryDetailComponent} from './dictionary/dictionary-detail/dictionary-detail.component';
+import {ProductUpdateComponent} from './product/product-update/product-update.component';
 import {HttpClientModule} from "@angular/common/http";
-import { HouseListComponent } from './house/house-list/house-list.component';
-import { HouseUpdateComponent } from './house/house-update/house-update.component';
-import { HouseCreateComponent } from './house/house-create/house-create.component';
-
+import {HouseListComponent} from './house/house-list/house-list.component';
+import {HouseUpdateComponent} from './house/house-update/house-update.component';
+import {HouseCreateComponent} from './house/house-create/house-create.component';
+import {ImageGalleryModule} from './image-gallery/image-gallery.module';
+import {GalleryConfig} from './image-gallery/token';
 @NgModule({
   declarations: [
     AppComponent,
@@ -71,9 +72,12 @@ import { HouseCreateComponent } from './house/house-create/house-create.componen
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ImageGalleryModule
+
   ],
-  providers: [],
+  providers: [{provide: GalleryConfig, useValue: 3}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
